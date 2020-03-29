b40899fd1fe370f773d94770752b199ead667c18
khloptsev <p.khloptsev@gmail.com>
2020-03-30 01:02:49 +1000

    add default func (show help used commander.js)

--- src/index.js
+++ src/index.js
@@ -1,1 +1,9 @@
-export default () => console.log('Start');
+import commander from 'commander';
+
+export default () => {
+  const program = commander;
+  program
+    .version('0.0.1')
+    .description('Compares two configuration files and shows a difference');
+  program.parse(process.argv);
+};
