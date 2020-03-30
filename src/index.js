ddc75781589e2be5053cd33a2a0d399c1641d498
khloptsev <p.khloptsev@gmail.com>
2020-03-30 11:39:40 +1000

    update main func

--- src/index.js
+++ src/index.js
@@ -1,7 +1,7 @@
-import commander from 'commander';
+import program from 'commander';
 
+// main
 export default () => {
-  const program = commander;
   program
     .version('0.0.1')
     .description('Compares two configuration files and shows a difference');
