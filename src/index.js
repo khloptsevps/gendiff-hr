f522c5440dd19e79ba5bb71dc6ea2be79b9ded4f
khloptsev <p.khloptsev@gmail.com>
2020-03-31 12:19:29 +1000

    add arguments, add option

--- src/index.js
+++ src/index.js
@@ -4,6 +4,9 @@ import program from 'commander';
 export default () => {
   program
     .version('0.0.1')
-    .description('Compares two configuration files and shows a difference');
+    .description('Compares two configuration files and shows a difference')
+    .arguments('<firstConfig> <secondConfig>')
+    .option('-f, --format [type]', 'output format');
   program.parse(process.argv);
+  if (program.format) console.log('Coming soon');
 };
