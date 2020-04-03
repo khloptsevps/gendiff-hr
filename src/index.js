2fe80ae8fe29d61cd4174b9fccdf150d4fa6f1f7
khloptsev <p.khloptsev@gmail.com>
2020-04-03 15:25:23 +1000

    add genDiff func and move commander logic in bin/gendiff.js

--- src/index.js
+++ src/index.js
@@ -1,12 +1,36 @@
-import program from 'commander';
+import path from 'path';
+import fs from 'fs';
+import { has } from 'lodash';
 
-// main
-export default () => {
-  program
-    .version('0.0.1')
-    .description('Compares two configuration files and shows a difference')
-    .arguments('<firstConfig> <secondConfig>')
-    .option('-f, --format [type]', 'output format');
-  program.parse(process.argv);
-  if (program.format) console.log('Coming soon');
+// genDiff
+const genDiff = (firstPath, secondPath) => {
+  const firstJson = JSON.parse(fs.readFileSync(path.resolve(firstPath)));
+  const secondJson = JSON.parse(fs.readFileSync(path.resolve(secondPath)));
+  const firstKeys = Object.keys(firstJson);
+  const secondKeys = Object.keys(secondJson);
+  const combineKeys = secondKeys.reduce((acc, item) => {
+    if (!has(firstJson, item)) {
+      acc.push(item);
+    }
+    return acc;
+  }, [...firstKeys]);
+  const difference = combineKeys.reduce((acc, key) => {
+    if (has(firstJson, key) && has(secondJson, key)) {
+      if (firstJson[key] === secondJson[key]) {
+        acc.push(`    ${key}: ${firstJson[key]}`);
+      } else {
+        acc.push(`  - ${key}: ${firstJson[key]}`);
+        acc.push(`  + ${key}: ${secondJson[key]}`);
+      }
+    } else if ((has(firstJson, key) && !has(secondJson, key))) {
+      console.log(key);
+      acc.push(`  - ${key}: ${firstJson[key]}`);
+    } else {
+      acc.push(`  + ${key}: ${secondJson[key]}`);
+    }
+    return acc;
+  }, []);
+  console.log(`{\n${difference.join('\n')}\n}`);
 };
+
+export default genDiff;
