b3f85d2559bede3d693484894d4d4c91185b6ee9
khloptsev <p.khloptsev@gmail.com>
2020-04-03 15:18:07 +1000

    update gendiff.js

--- src/bin/gendiff.js
+++ src/bin/gendiff.js
@@ -1,4 +1,12 @@
 #!/usr/bin/env node
-import gendiff from '../index.js';
+import program from 'commander';
+import genDiff from '../index';
 
-gendiff();
+program
+  .version('0.0.1')
+  .description('Compares two configuration files and shows a difference')
+  .arguments('<firstConfig> <secondConfig>')
+  .option('-f, --format [type]', 'output format')
+  .action(genDiff);
+program.parse(process.argv);
+if (program.format) console.log('Coming soon');
