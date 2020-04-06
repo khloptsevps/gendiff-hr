87b8783dec817fa12fdc4de9eadc6b03c451d86d
khloptsev <p.khloptsev@gmail.com>
2020-04-07 09:24:12 +1000

    update tests

--- __test__/index.test.js
+++ __test__/index.test.js
@@ -8,16 +8,6 @@ const secondPathToJson = path.resolve(__dirname, '../__fixtures__/after.json');
 const firstPathtoYml = path.resolve(__dirname, '../__fixtures__/before.yml');
 const secondPathtoYml = path.resolve(__dirname, '../__fixtures__/after.yml');
 
-test('FilePathsToJson', () => {
-  expect(firstPathToJson).toEqual('/Users/khloptsev/JS/project/gendiff-hr/__fixtures__/before.json');
-  expect(secondPathToJson).toEqual('/Users/khloptsev/JS/project/gendiff-hr/__fixtures__/after.json');
-});
-
-test('FilePathsToYml', () => {
-  expect(firstPathtoYml).toEqual('/Users/khloptsev/JS/project/gendiff-hr/__fixtures__/before.yml');
-  expect(secondPathtoYml).toEqual('/Users/khloptsev/JS/project/gendiff-hr/__fixtures__/after.yml');
-});
-
 test('plainDiffTwoJsonFiles', () => {
   const diff = `{
     host: hexlet.io
