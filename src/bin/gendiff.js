b127084a5374ebf61e28e6c16eae2d8d8f013869
khloptsev <p.khloptsev@gmail.com>
2020-04-06 17:41:50 +1000

    update action section

--- src/bin/gendiff.js
+++ src/bin/gendiff.js
@@ -2,11 +2,12 @@
 import program from 'commander';
 import genDiff from '../index';
 
+
 program
   .version('0.0.1')
   .description('Compares two configuration files and shows a difference')
   .arguments('<firstConfig> <secondConfig>')
   .option('-f, --format [type]', 'output format')
-  .action(genDiff);
+  .action((firstConfig, secondConfig) => console.log(genDiff(firstConfig, secondConfig)));
 program.parse(process.argv);
 if (program.format) console.log('Coming soon');
