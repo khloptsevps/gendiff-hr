b3766066b0f34f00f209ac7727ce65634b201c91
khloptsev <p.khloptsev@gmail.com>
2020-04-07 12:26:56 +1000

    update tests

--- __test__/index.test.js
+++ __test__/index.test.js
@@ -11,11 +11,14 @@ const diff = `{
   + verbose: true
 }`;
 
-const firstPathToJson = path.resolve(__dirname, '../__fixtures__/before.json');
-const secondPathToJson = path.resolve(__dirname, '../__fixtures__/after.json');
+const firstPathToJson = path.resolve(__dirname, './fixtures/before.json');
+const secondPathToJson = path.resolve(__dirname, './fixtures/after.json');
 
-const firstPathtoYml = path.resolve(__dirname, '../__fixtures__/before.yml');
-const secondPathtoYml = path.resolve(__dirname, '../__fixtures__/after.yml');
+const firstPathtoYml = path.resolve(__dirname, './fixtures/before.yml');
+const secondPathtoYml = path.resolve(__dirname, './fixtures/after.yml');
+
+const firstPathtoIni = path.resolve(__dirname, './fixtures/before.ini');
+const secondPathtoIni = path.resolve(__dirname, './fixtures/after.ini');
 
 test('plainDiffTwoJsonFiles', () => {
   expect(genDiff(firstPathToJson, secondPathToJson)).toEqual(diff);
@@ -23,4 +26,8 @@ test('plainDiffTwoJsonFiles', () => {
 
 test('plainDiffTwoYmlFiles', () => {
   expect(genDiff(firstPathtoYml, secondPathtoYml)).toEqual(diff);
+});
+
+test('plainDiffTwoIniFiles', () => {
+  expect(genDiff(firstPathtoIni, secondPathtoIni)).toEqual(diff);
 });
