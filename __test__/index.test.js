afb5c21087de95e514aa7322808466dc6db149d4
khloptsev <p.khloptsev@gmail.com>
2020-04-07 10:16:21 +1000

    update test

--- __test__/index.test.js
+++ __test__/index.test.js
@@ -2,6 +2,15 @@
 import path from 'path';
 import genDiff from '../src/index.js';
 
+const diff = `{
+    host: hexlet.io
+  - timeout: 50
+  + timeout: 20
+  - proxy: 123.234.53.22
+  - follow: false
+  + verbose: true
+}`;
+
 const firstPathToJson = path.resolve(__dirname, '../__fixtures__/before.json');
 const secondPathToJson = path.resolve(__dirname, '../__fixtures__/after.json');
 
@@ -9,25 +18,9 @@ const firstPathtoYml = path.resolve(__dirname, '../__fixtures__/before.yml');
 const secondPathtoYml = path.resolve(__dirname, '../__fixtures__/after.yml');
 
 test('plainDiffTwoJsonFiles', () => {
-  const diff = `{
-    host: hexlet.io
-  - timeout: 50
-  + timeout: 20
-  - proxy: 123.234.53.22
-  - follow: false
-  + verbose: true
-}`;
   expect(genDiff(firstPathToJson, secondPathToJson)).toEqual(diff);
 });
 
 test('plainDiffTwoYmlFiles', () => {
-  const diff = `{
-    host: hexlet.io
-  - timeout: 50
-  + timeout: 20
-  - proxy: 123.234.53.22
-  - follow: false
-  + verbose: true
-}`;
   expect(genDiff(firstPathtoYml, secondPathtoYml)).toEqual(diff);
 });
