bc88d3bfb5d33925142c6fac3d5c12d29e201306
khloptsev <p.khloptsev@gmail.com>
2020-04-07 12:23:41 +1000

    add ini.parse, update getConfigPath

--- src/parsers.js
+++ src/parsers.js
@@ -1,8 +1,9 @@
 import path from 'path';
 import fs from 'fs';
 import yaml from 'js-yaml';
+import ini from 'ini';
 
-const getConfigPath = (pathToConfig) => fs.readFileSync(path.resolve(pathToConfig));
+const getConfigPath = (pathToConfig) => fs.readFileSync(path.resolve(pathToConfig), 'utf-8');
 
 export const parsing = (config) => {
   switch (path.extname(config)) {
@@ -10,6 +11,8 @@ export const parsing = (config) => {
       return JSON.parse(getConfigPath(config));
     case '.yml':
       return yaml.safeLoad(getConfigPath(config));
+    case '.ini':
+      return ini.parse(getConfigPath(config));
     default:
       throw new Error('Unknown file extention');
   }
