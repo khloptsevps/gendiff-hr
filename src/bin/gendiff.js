f1ff9924164ef74441f4eb17de1bf9492840b988
khloptsev <p.khloptsev@gmail.com>
2020-04-20 16:35:55 +1000

    add default format in program

--- src/bin/gendiff.js
+++ src/bin/gendiff.js
@@ -7,7 +7,7 @@ program
   .version('0.0.1')
   .description('Compares two configuration files and shows a difference')
   .arguments('<firstConfig> <secondConfig>')
-  .option('-f, --format [type]', 'output format')
-  .action((firstConfig, secondConfig) => console.log(genDiff(firstConfig, secondConfig)));
-program.parse(process.argv);
-if (program.format) console.log('Coming soon');
+  .option('-f, --format [type]', 'output format', 'plain')
+  .action((firstConfig, secondConfig) => (
+    console.log(genDiff(firstConfig, secondConfig, program.format))))
+  .parse(process.argv);
