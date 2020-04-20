cbbdf76363bbd4f78126a9e056343d100bee9b2a
khloptsev <p.khloptsev@gmail.com>
2020-04-20 16:34:55 +1000

    add plain render for palin and recursive struсtures

--- src/index.js
+++ src/index.js
@@ -1,34 +1,78 @@
-import { has } from 'lodash';
+import { has, union, isObject } from 'lodash';
 import { parsing } from './parsers.js';
 
-// genDiff
-const genDiff = (firstPath, secondPath) => {
-  const firstConfig = parsing(firstPath);
-  const secondConfig = parsing(secondPath);
-  const firstKeys = Object.keys(firstConfig);
-  const secondKeys = Object.keys(secondConfig);
-  const combineKeys = secondKeys.reduce((acc, item) => {
-    if (!has(firstConfig, item)) {
-      acc.push(item);
-    }
-    return acc;
-  }, [...firstKeys]);
-  const difference = combineKeys.reduce((acc, key) => {
-    if (has(firstConfig, key) && has(secondConfig, key)) {
-      if (firstConfig[key] === secondConfig[key]) {
-        acc.push(`    ${key}: ${firstConfig[key]}`);
-      } else {
-        acc.push(`  - ${key}: ${firstConfig[key]}`);
-        acc.push(`  + ${key}: ${secondConfig[key]}`);
+// diffTree
+export const makeDifferenceTree = (beforeConfig, afterConfig) => {
+  const keys = union(Object.keys(beforeConfig), Object.keys(afterConfig));
+  const result = keys.reduce((acc, key) => {
+    if (has(beforeConfig, key) && has(afterConfig, key)) {
+      if (isObject(beforeConfig[key]) && isObject(afterConfig[key])) {
+        acc.push({ name: key, children: makeDifferenceTree(beforeConfig[key], afterConfig[key]) });
+        return acc;
       }
-    } else if ((has(firstConfig, key) && !has(secondConfig, key))) {
-      acc.push(`  - ${key}: ${firstConfig[key]}`);
+      acc.push(beforeConfig[key] === afterConfig[key] ? {
+        name: key,
+        status: 'unmodified',
+        value: afterConfig[key],
+      } : {
+        name: key,
+        status: 'modified',
+        before: beforeConfig[key],
+        after: afterConfig[key],
+      });
+    } else if (has(beforeConfig, key) && !has(afterConfig, key)) {
+      acc.push({ name: key, status: 'deleted', value: beforeConfig[key] });
     } else {
-      acc.push(`  + ${key}: ${secondConfig[key]}`);
+      acc.push({ name: key, status: 'added', value: afterConfig[key] });
     }
     return acc;
   }, []);
-  return `{\n${difference.join('\n')}\n}`;
+  return result;
+};
+
+const stringify = (data, lvl = 0) => {
+  if (!isObject(data)) {
+    return data;
+  }
+  const keys = Object.keys(data);
+  const result = keys.map((key) => (`{\n${' '.repeat(lvl)}${key}: ${stringify(data[key])}\n${' '.repeat(lvl - 4)}}`));
+  return result;
+};
+
+const plain = (tree, space = 4) => {
+  const result = tree.map((node) => {
+    if (node.status === 'unmodified') {
+      const spaceCount = space;
+      return `${' '.repeat(spaceCount) + node.name}: ${stringify(node.value, space + 4)}`;
+    }
+    if (node.status === 'modified') {
+      const spaceCount = space - 2;
+      return [
+        `${' '.repeat(spaceCount)}- ${node.name}: ${stringify(node.before, space + 4)}`,
+        `${' '.repeat(spaceCount)}+ ${node.name}: ${stringify(node.after, space + 4)}`,
+      ].join('\n');
+    }
+    if (node.status === 'added') {
+      const spaceCount = space - 2;
+      return `${' '.repeat(spaceCount)}+ ${node.name}: ${stringify(node.value, space + 4)}`;
+    }
+    if (node.status === 'deleted') {
+      const spaceCount = space - 2;
+      return `${' '.repeat(spaceCount)}- ${node.name}: ${stringify(node.value, space + 4)}`;
+    }
+    return `${' '.repeat(space)}${node.name}: {\n${plain(node.children, space + 4)}\n${' '.repeat(space)}}`;
+  });
+  return `${result.join('\n')}`;
+};
+
+const genDiff = (firstConfig, secondConfig, format) => {
+  const diffTree = makeDifferenceTree(parsing(firstConfig), parsing(secondConfig));
+  switch (format) {
+    case 'pretty':
+      return 'work in process';
+    default:
+      return `{\n${plain(diffTree)}\n}`;
+  }
 };
 
 export default genDiff;
