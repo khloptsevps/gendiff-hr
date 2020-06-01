bf69ed62dcd0b393ccd1fc5e785383ea476bb074
khloptsev <p.khloptsev@gmail.com>
2020-06-01 20:16:01 +1000

    update diffTree func

--- src/index.js
+++ src/index.js
@@ -1,48 +1,56 @@
-import { has, union, isObject } from 'lodash';
-import stylish from './formatters/stylish.js';
-import json from './formatters/json.js';
-import plain from './formatters/plain.js';
+import { has, isObject } from 'lodash';
+import formatterChoose from './formatters/index.js';
 import { parsing } from './parsers.js';
 
 // diffTree
 export const makeDifferenceTree = (beforeConfig, afterConfig) => {
-  const keys = union(Object.keys(beforeConfig), Object.keys(afterConfig));
-  const result = keys.reduce((acc, key) => {
+  const mergedObject = { ...beforeConfig, ...afterConfig };
+  const entries = Object.entries(mergedObject);
+  const result = entries.map(([key, value]) => {
+    const property = key;
+    const oldValue = beforeConfig[key];
+    const newValue = afterConfig[key];
     if (has(beforeConfig, key) && has(afterConfig, key)) {
-      if (isObject(beforeConfig[key]) && isObject(afterConfig[key])) {
-        acc.push({ name: key, children: makeDifferenceTree(beforeConfig[key], afterConfig[key]) });
-        return acc;
+      if (isObject(oldValue) && isObject(newValue)) {
+        return {
+          property,
+          status: 'merged',
+          children: makeDifferenceTree(oldValue, newValue),
+        };
       }
-      acc.push(beforeConfig[key] === afterConfig[key] ? {
-        name: key,
-        status: 'unmodified',
-        value: afterConfig[key],
-      } : {
-        name: key,
+      if (oldValue === newValue) {
+        return {
+          property,
+          status: 'unmodified',
+          value,
+        };
+      }
+      return {
+        property,
         status: 'modified',
-        before: beforeConfig[key],
-        after: afterConfig[key],
-      });
-    } else if (has(beforeConfig, key) && !has(afterConfig, key)) {
-      acc.push({ name: key, status: 'deleted', value: beforeConfig[key] });
-    } else {
-      acc.push({ name: key, status: 'added', value: afterConfig[key] });
+        oldValue,
+        newValue,
+      };
     }
-    return acc;
-  }, []);
+    if (has(beforeConfig, key) && !has(afterConfig, key)) {
+      return {
+        property,
+        status: 'deleted',
+        value,
+      };
+    }
+    return {
+      property,
+      status: 'added',
+      value,
+    };
+  });
   return result;
 };
 
 const genDiff = (firstConfig, secondConfig, format) => {
   const diffTree = makeDifferenceTree(parsing(firstConfig), parsing(secondConfig));
-  switch (format) {
-    case 'plain':
-      return `${plain(diffTree, [])}`;
-    case 'json':
-      return json(diffTree);
-    default:
-      return stylish(diffTree);
-  }
+  return formatterChoose(diffTree, format);
 };
 
 export default genDiff;
