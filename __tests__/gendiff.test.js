e3796558b626a568f7bd88b8059941e1dafd3ae3
khloptsev <p.khloptsev@gmail.com>
2020-06-01 20:11:22 +1000

    update test and update fixtures

--- __tests__/gendiff.test.js
+++ __tests__/gendiff.test.js
@@ -2,49 +2,28 @@ import path from 'path';
 import fs from 'fs';
 import genDiff from '../src/index.js';
 
-const flatResult = fs.readFileSync(path.resolve(__dirname, './fixtures/flat/result.diff'), 'utf8');
-const recurciveResult = fs.readFileSync(path.resolve(__dirname, './fixtures/recursive/result.diff'), 'utf8');
-const plainResult = fs.readFileSync(path.resolve(__dirname, './fixtures/recursive/plain.diff'), 'utf8');
-const jsonResult = fs.readFileSync(path.resolve(__dirname, './fixtures/recursive/json.diff'), 'utf8');
+const extention = ['json', 'yml', 'ini'];
 
-let beforeJSON = path.resolve(__dirname, './fixtures/flat/before.yml');
-let afterJSON = path.resolve(__dirname, './fixtures/flat/after.yml');
+describe('gendiff', () => {
+  const recurciveResult = fs.readFileSync(path.resolve(__dirname, './fixtures/recursive/result.diff'), 'utf8');
+  const plainResult = fs.readFileSync(path.resolve(__dirname, './fixtures/recursive/plain.diff'), 'utf8');
+  const jsonResult = fs.readFileSync(path.resolve(__dirname, './fixtures/recursive/json.diff'), 'utf8');
 
-let beforeYML = path.resolve(__dirname, './fixtures/flat/before.yml');
-let afterYML = path.resolve(__dirname, './fixtures/flat/after.yml');
+  describe.each(extention)('compare two %s files', (ext) => {
+    const before = path.resolve(__dirname, `./fixtures/recursive/before.${ext}`);
+    const after = path.resolve(__dirname, `./fixtures/recursive/after.${ext}`);
+    const expected = genDiff(before, after);
 
-let beforeINI = path.resolve(__dirname, './fixtures/flat/before.yml');
-let afterINI = path.resolve(__dirname, './fixtures/flat/after.yml');
+    test('stylish', () => {
+      expect(expected).toEqual(recurciveResult);
+    });
 
-afterEach(() => {
-  beforeJSON = path.resolve(__dirname, './fixtures/recursive/before.json');
-  afterJSON = path.resolve(__dirname, './fixtures/recursive/after.json');
-  beforeYML = path.resolve(__dirname, './fixtures/recursive/before.json');
-  afterYML = path.resolve(__dirname, './fixtures/recursive/after.json');
-  beforeINI = path.resolve(__dirname, './fixtures/recursive/before.json');
-  afterINI = path.resolve(__dirname, './fixtures/recursive/after.json');
-});
+    test('plain', () => {
+      expect(genDiff(before, after, 'plain')).toEqual(plainResult);
+    });
 
-test('flat', () => {
-  expect(genDiff(beforeJSON, afterJSON)).toEqual(flatResult);
-  expect(genDiff(beforeYML, afterYML)).toEqual(flatResult);
-  expect(genDiff(beforeINI, afterINI)).toEqual(flatResult);
-});
-
-test('recursive', () => {
-  expect(genDiff(beforeJSON, afterJSON)).toEqual(recurciveResult);
-  expect(genDiff(beforeYML, afterYML)).toEqual(recurciveResult);
-  expect(genDiff(beforeINI, afterINI)).toEqual(recurciveResult);
-});
-
-test('plain', () => {
-  expect(genDiff(beforeJSON, afterJSON, 'plain')).toEqual(plainResult);
-  expect(genDiff(beforeYML, afterYML, 'plain')).toEqual(plainResult);
-  expect(genDiff(beforeINI, afterINI, 'plain')).toEqual(plainResult);
-});
-
-test('json', () => {
-  expect(genDiff(beforeJSON, afterJSON, 'json')).toEqual(jsonResult);
-  expect(genDiff(beforeYML, afterYML, 'json')).toEqual(jsonResult);
-  expect(genDiff(beforeINI, afterINI, 'json')).toEqual(jsonResult);
+    test('tree', () => {
+      expect(genDiff(before, after, 'json')).toEqual(jsonResult);
+    });
+  });
 });
