911fe18b8f26cb4dc7b87f7463662fb2f19f1341
khloptsev <p.khloptsev@gmail.com>
2020-06-16 22:24:25 +1000

    update makeDifferenceTree, add getFileData func, update genDiff func

--- src/index.js
+++ src/index.js
@@ -1,56 +1,71 @@
 import { has, isObject } from 'lodash';
-import formatterChoose from './formatters/index.js';
-import { parsing } from './parsers.js';
+import fs from 'fs';
+import path from 'path';
+import getFormatter from './formatters/index.js';
+import { parse } from './parsers.js';
 
 // diffTree
 export const makeDifferenceTree = (beforeConfig, afterConfig) => {
-  const mergedObject = { ...beforeConfig, ...afterConfig };
-  const entries = Object.entries(mergedObject);
-  const result = entries.map(([key, value]) => {
-    const property = key;
-    const oldValue = beforeConfig[key];
-    const newValue = afterConfig[key];
-    if (has(beforeConfig, key) && has(afterConfig, key)) {
-      if (isObject(oldValue) && isObject(newValue)) {
-        return {
-          property,
-          status: 'merged',
-          children: makeDifferenceTree(oldValue, newValue),
-        };
-      }
-      if (oldValue === newValue) {
-        return {
-          property,
-          status: 'unmodified',
-          value,
-        };
-      }
+  const entries = Object.entries({ ...beforeConfig, ...afterConfig });
+  const result = entries.map(([property, value]) => {
+    const oldValue = beforeConfig[property];
+    const newValue = afterConfig[property];
+    if (isObject(oldValue) && isObject(newValue)) {
       return {
         property,
-        status: 'modified',
-        oldValue,
-        newValue,
+        status: 'merged',
+        children: makeDifferenceTree(oldValue, newValue),
       };
     }
-    if (has(beforeConfig, key) && !has(afterConfig, key)) {
+    if (has(afterConfig, property) && !has(beforeConfig, property)) {
+      return {
+        property,
+        status: 'added',
+        value,
+      };
+    }
+    if (has(beforeConfig, property) && !has(afterConfig, property)) {
       return {
         property,
         status: 'deleted',
         value,
       };
     }
+    if (oldValue === newValue) {
+      return {
+        property,
+        status: 'unmodified',
+        value,
+      };
+    }
     return {
       property,
-      status: 'added',
-      value,
+      status: 'modified',
+      oldValue,
+      newValue,
     };
   });
   return result;
 };
 
-const genDiff = (firstConfig, secondConfig, format) => {
-  const diffTree = makeDifferenceTree(parsing(firstConfig), parsing(secondConfig));
-  return formatterChoose(diffTree, format);
+const getFileData = (pathToFile) => {
+  const data = fs.readFileSync(path.resolve(pathToFile), 'utf-8');
+  const ext = path.extname(pathToFile);
+
+  return { data, ext };
+};
+
+const genDiff = (pathToFile1, pathToFile2, format) => {
+  const beforeConfig = getFileData(pathToFile1);
+  const afterConfig = getFileData(pathToFile2);
+
+  const parseBefore = parse(beforeConfig.ext, beforeConfig.data);
+  const parseAfter = parse(afterConfig.ext, afterConfig.data);
+
+  const diffTree = makeDifferenceTree(parseBefore, parseAfter);
+  const result = getFormatter(diffTree, format);
+
+  return result;
 };
 
 export default genDiff;
