fb625325d1ebc373887f608a7c661d1709280b7a
khloptsev <p.khloptsev@gmail.com>
2020-06-23 15:40:41 +1000

    create myIniParse, update parse

--- src/parsers.js
+++ src/parsers.js
@@ -2,25 +2,26 @@ import yaml from 'js-yaml';
 import ini from 'ini';
 import { isObject, isBoolean, keys } from 'lodash';
 
-const normalize = (config) => {
-  const configKeys = keys(config);
-  const result = configKeys.reduce((acc, key) => {
-    if (isObject(config[key])) {
-      return { ...acc, [key]: normalize(config[key]) };
-    }
-    if (isBoolean(config[key])) {
-      return { ...acc, [key]: config[key] };
-    }
-    if (Number.isNaN(Number(config[key]))) {
-      return { ...acc, [key]: config[key] };
-    }
-    return { ...acc, [key]: Number(config[key]) };
-  }, {});
-  return result;
-};
-
 const myIniParse = (fileData) => {
   const parseFile = ini.parse(fileData);
+
+  const normalize = (config) => {
+    const configKeys = keys(config);
+    const result = configKeys.reduce((acc, key) => {
+      if (isObject(config[key])) {
+        return { ...acc, [key]: normalize(config[key]) };
+      }
+      if (isBoolean(config[key])) {
+        return { ...acc, [key]: config[key] };
+      }
+      if (Number.isNaN(Number(config[key]))) {
+        return { ...acc, [key]: config[key] };
+      }
+      return { ...acc, [key]: Number(config[key]) };
+    }, {});
+    return result;
+  };
+
   return normalize(parseFile);
 };
 
