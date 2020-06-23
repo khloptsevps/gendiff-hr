a4072ca1dbda16d46d9aa895143cf3bcfc869ea9
khloptsev <p.khloptsev@gmail.com>
2020-06-23 12:37:33 +1000

    update default func

--- src/formatters/index.js
+++ src/formatters/index.js
@@ -1,16 +1,13 @@
 import plain from './plain.js';
-import json from './json.js';
 import stylish from './stylish.js';
 
-const buildFormat = (tree, format) => {
+export default (tree, format) => {
   switch (format) {
     case 'plain':
       return plain(tree);
     case 'json':
-      return json(tree);
+      return JSON.stringify(tree);
     default:
       return stylish(tree);
   }
 };
-
-export default buildFormat;
