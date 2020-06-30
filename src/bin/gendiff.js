377c18889b2470dc390c46a4adb5b8981d1db6ff
khloptsev <p.khloptsev@gmail.com>
2020-06-30 11:23:34 +1000

    fix option

--- src/bin/gendiff.js
+++ src/bin/gendiff.js
@@ -7,7 +7,7 @@ program
   .version('0.0.1')
   .description('Compares two configuration files and shows a difference')
   .arguments('<firstConfig> <secondConfig>')
-  .option('-f, --format [type]', 'output format', 'tree')
+  .option('-f, --format [type]', 'output format: json, plain, tree', 'tree')
   .action((firstConfig, secondConfig) => (
     console.log(genDiff(firstConfig, secondConfig, program.format))))
   .parse(process.argv);
